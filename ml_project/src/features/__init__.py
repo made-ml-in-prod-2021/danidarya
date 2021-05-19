cfcd7e8a23891026f884692451d07622a3b44390
Daria <69820188+danidarya@users.noreply.github.com>
2021-05-19 16:44:40 +0300

    Delete ml_project directory

--- ml_project/src/features/__init__.py
+++
@@ -1,8 +0,0 @@
-from .make_features import make_features, build_transformer, extract_target, process_categorical_features,process_numerical_features
-
-__all__ = [
-    "make_features",
-    "build_transformer",
-    "extract_target",
-    "process_categorical_features"
-]
